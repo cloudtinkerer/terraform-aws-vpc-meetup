65767c095bf0ee63c55ec78912976d5c46ff5295
Carlo Cayos <carlocayos@gmail.com>
2018-04-11 15:29:00 +1000

    Exercise 06

--- main.tf
+++ main.tf
@@ -134,3 +134,15 @@ resource "aws_security_group" "sg_instance" {
 #  - Create a "Name" tag - (tags)
 #
 #-------------------------------------------
+resource "aws_instance" "nginx" {
+  ami           = "ami-fc41829e"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = ["${aws_security_group.sg_instance.id}"]
+  subnet_id              = "${element(aws_subnet.public_subnet.*.id, 1)}"
+  associate_public_ip_address = true
+
+  tags {
+    Name = "my-nginx-instance-${terraform.workspace}"
+  }
+}
